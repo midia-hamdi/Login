617ecc2d39629410ff1b7fd65ba328cac8c0b78b
midia-hamdi <midiahamdi367@gmail.com>
2023-11-23 15:40:40 +0330

    15-useRef vs createRef

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
 export default function LogIn() {
+    const UsernamrRef = useRef();
+
+    useEffect(() => {
+        UsernamrRef.current.focus()
+    })
   return (
     <div className='login'>
         <h1>Login</h1>
         <form method='post'>
-            <input type='text' name='u' placeholder='Username' required='required' />
+            <input  ref={UsernamrRef} type='text' name='u' placeholder='Username' required='required' />
             <input type='password' name='p' placeholder='Password' required='required' />
             <button type='submit' className='btn btn-primary btn-block btn-large'>Let me in.</button>
         </form>
